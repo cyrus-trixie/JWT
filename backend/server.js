e8d836f947bb676da0a4af9022d0adc01bb3d06e
Cyrus Ngugi <cmuiruringugi@gmail.com>
2026-06-08 22:42:58 +0300

    i added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
-
+const supabase = require('./supabaseClient');
+const app= require('express');
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
